9cecb9ecf5d6f13897da85e88a3732ae1ddc13d2
Adithya Gurram <adittya234@gmail.com>
2025-01-03 01:52:27 +0530

    Initial commit

---
+++ src/test/java/com/finance/tracker/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.tracker.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
